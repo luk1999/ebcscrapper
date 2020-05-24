91d75e38f92904b478135f4640588a73b48dd464
Łukasz Stuszek <lukasz.stuszek@nx-labs.com>
2020-05-24 20:54:44 +0200

    Currency and CurrencyRate models added, migration created.

--- scrapper/models.py
+++ scrapper/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    symbol = models.CharField(max_length=3)
+    name = models.CharField(max_length=30)
+
+
+class CurrencyRate(models.Model):
+    main_currency = models.ForeignKey(
+        Currency,
+        on_delete=models.PROTECT,
+        related_name='main_currency'
+    )
+    currency = models.ForeignKey(
+        Currency,
+        on_delete=models.PROTECT,
+        related_name='currency'
+    )
+    rate = models.DecimalField(max_digits=10, decimal_places=6)
+    updated_at = models.DateField()
